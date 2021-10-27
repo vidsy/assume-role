d8c6492087711eafbab2cd980e4957188a35511a
Steven Jack <steve@vidsy.co>
2021-10-27 12:57:38 +0100

    Update terraform wrapper to only run init command when user runs it (#35)

--- terraform-wrapper.sh
+++ terraform-wrapper.sh
@@ -6,39 +6,30 @@ if [ "$TIME_LEFT" -lt "0" ]; then
   exit -1
 fi
 
-case $1 in
-plan|apply|destroy|refresh)
-
+function var_file_path() {
   if [ -z "$VAR_FILE" ]; then
-    VAR_FILE="-var-file=$AWS_ENV.tfvars"
+    echo "-var-file=$AWS_ENV.tfvars"
 
     if [ ! -f $AWS_ENV.tfvars ]; then
-      VAR_FILE=""
+      echo ""
     fi
   fi
+}
 
-  if [ -z "$STATE_FILE" ]; then
-    STATE_FILE="${PWD##*/}"
+VAR_FILE="$(var_file_path)"
 
-    if [ -f .terraform_config ]; then
-      STATE_FILE=$(cat .terraform_config | jq -r ".remote.key")
-    fi
-  fi
-
-  rm -rf .terraform/terraform.tfstate*
-  terraform init -backend=true -backend-config="bucket=$TERRAFORM_STATE_BUCKET" -backend-config="region=$AWS_REGION"
+case $1 in
+plan|apply|destroy|refresh)
   terraform.real $@ $VAR_FILE
+  ;;
+import)
+  terraform.real import $VAR_FILE ${@:2}
+  ;;
+init)
+  rm -rf .terraform/terraform.tfstate*
+  terraform.real init -backend=true -backend-config="bucket=$TERRAFORM_STATE_BUCKET" -backend-config="region=$AWS_REGION"
   ;;
 *)
   terraform.real $@
-  ;;
-import)
-  if [ -z "$VAR_FILE" ]; then
-    VAR_FILE="-var-file=$AWS_ENV.tfvars"
-
-    if [ ! -f $AWS_ENV.tfvars ]; then
-      VAR_FILE=""
-    fi
-  fi
   ;;
 esac
